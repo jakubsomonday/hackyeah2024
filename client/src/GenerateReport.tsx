cdc3b3307bf78cf7e21736919b040d0ad2fe393d
Jakub Sokol <jakubso@monday.com>
2024-09-28 22:29:45 +0200

    add icon

--- client/src/GenerateReport.tsx
+++ client/src/GenerateReport.tsx
@@ -10,7 +10,8 @@ const GenerateReport: React.FC<GenerateReportProps> = ({ imageUrl, onGenerateRep
   const aggregatedCount = 10;
   return (
     <div className='box is-flex is-align-items-center'>
-      <div className='image' style={{ width: '150px', height: '150px', marginRight: '1.5rem' }}>
+      <div className='image' style={{ width: '150px', height: '150px', marginLeft: '1.5rem' }}>
+        <i className="fa-solid fa-file-invoice is-align-items-center" style={{ fontSize: '9em' }}></i>
       </div>
       <div className='content'>
         <h2 className='title is-4'>Generuj Raport</h2>
